d54e8d17601166d414f90838c16ed0e5d296ec3d
Traore Mohamed <traoremohamed02n02@gmail.com>
2026-06-04 10:20:00 +0100

    Initial commit - structure du projet

---
+++ src/main/java/com/timz/rag_platform/RagPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.timz.rag_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timz/rag_platform/RagPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timz.rag_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
