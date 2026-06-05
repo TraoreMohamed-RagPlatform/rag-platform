c4c9713f76b8455647ec5ebff164d0f536409af6
Traore Mohamed <traoremohamed02n02@gmail.com>
2026-06-05 16:03:32 +0100

    Add login page + dashboard - J2

---
+++ src/main/java/com/timz/rag_platform/HomeController.java
@@ -0,0 +1,18 @@
+package com.timz.rag_platform;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "auth/login";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard() {
+        return "dashboard";
+    }
+}
